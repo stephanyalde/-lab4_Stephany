3434c50c379220a022b634bff680dcd7728e69ad
Alcaide <salcaid@ilstu.edu>
2026-02-05 14:55:25 -0600

    new stuff

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.listen(PORT, () => {
   console.log('Server running on port', PORT);
 });
 app.get('/', (req, res) => {
-  res.send('Hello from Node + Express');
+  res.send('Commiting Changesssss');
 });
 app.get('/api/health', (req, res) => {
   res.json({ status: 'ok', time: new Date().toISOString() });
